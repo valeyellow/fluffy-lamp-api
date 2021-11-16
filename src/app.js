d833288100e419aa64e953b3ad905f67420e5e20
valeyellow <sumit.mishtra.4693@gmail.com>
2021-11-16 15:35:31 +0530

    feat: Setup express app

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+dotenv.config();
+
+const port = process.env.PORT || 8080;
+
+app.get("/healthcheck", async (req, res) => {
+  res.send("Connection successful!");
+});
+
+app.listen(port, async () => {
+  console.log(`App is listening on port ${port}`);
+});
