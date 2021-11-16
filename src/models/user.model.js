c5cdee5de1c945296acaeff11926cf0136c271e5
valeyellow <sumit.mishtra.4693@gmail.com>
2021-11-16 16:04:10 +0530

    feat: Add User model

---
+++ src/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
