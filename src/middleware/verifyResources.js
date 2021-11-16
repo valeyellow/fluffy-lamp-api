9ae26964a0bbf4df925c8e2660a11d4e1665e723
valeyellow <sumit.mishtra.4693@gmail.com>
2021-11-16 16:16:51 +0530

    feat: Add middleware to parse request

---
+++ src/middleware/verifyResources.js
@@ -0,0 +1,21 @@
+/**
+ * Middleware to parse input schema object
+ * @param {AnyZodObject} schema
+ * @returns
+ * if successful, invokes the next function
+ * else, returns the error as response
+ */
+const validate = (schema) => (req, res, next) => {
+  try {
+    schema.parse({
+      body: req.body,
+      query: req.query,
+      params: req.params,
+    });
+    next();
+  } catch (error) {
+    res.status(400).json({ type: error, message: error });
+  }
+};
+
+module.exports = validate;
