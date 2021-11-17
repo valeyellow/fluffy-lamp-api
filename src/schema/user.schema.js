a4dbcc2f0f84a10c2d2681275641f7371f5bdbdb
valeyellow <sumit.mishtra.4693@gmail.com>
2021-11-17 15:28:53 +0530

    feat(user): Add user schema
    Create user schema to validate input

---
+++ src/schema/user.schema.js
@@ -0,0 +1,17 @@
+const { object, string } = require("zod");
+
+const createUserSchema = object({
+  body: object({
+    email: string({ required_error: "Email is a required field" }).email(
+      "Please enter a valid email"
+    ),
+    firstName: string({
+      required_error: "First name cannot be empty",
+    }),
+    lastName: string({
+      required_error: "Last name cannot be empty",
+    }),
+  }),
+});
+
+module.exports = createUserSchema;
