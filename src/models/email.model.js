fa32629686cfca08cddd5e18a61c543feb48d699
valeyellow <sumit.mishtra.4693@gmail.com>
2021-11-17 20:31:07 +0530

    feat(emailModel): Add timestamps

--- src/models/email.model.js
+++ src/models/email.model.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 
-const emailSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    trim: true,
+const emailSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
   },
-  isVerified: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true },
+);
 
 const EmailModel = mongoose.model("Email", emailSchema);
 
