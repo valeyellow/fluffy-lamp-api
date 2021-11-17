f622327bb2f6784a33312e9d90b2c60357f6f3fc
valeyellow <sumit.mishtra.4693@gmail.com>
2021-11-17 20:46:53 +0530

    refactor(user): Fix dangling comma

--- src/schema/user.schema.js
+++ src/schema/user.schema.js
@@ -3,7 +3,7 @@ const { object, string } = require("zod");
 const createUserSchema = object({
   body: object({
     email: string({ required_error: "Email is a required field" }).email(
-      "Please enter a valid email"
+      "Please enter a valid email",
     ),
     firstName: string({
       required_error: "First name cannot be empty",
