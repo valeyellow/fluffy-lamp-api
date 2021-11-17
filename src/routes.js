546734fa0eb9c27a89391f487d83d198ee574116
valeyellow <sumit.mishtra.4693@gmail.com>
2021-11-17 20:47:56 +0530

    refactor: Remove unused resources

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,10 @@
 const userRouter = require("./routers/user.router");
 const postRouter = require("./routers/post.router");
-const commentRouter = require("./routers/comment.router");
 const authRouter = require("./routers/auth.router");
 
 const routes = async (app) => {
   app.use(userRouter);
   app.use(postRouter);
-  app.use(commentRouter);
   app.use(authRouter);
 };
 
