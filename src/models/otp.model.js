e45f5788868cf18a65773f56024ae378681378ea
valeyellow <sumit.mishtra.4693@gmail.com>
2021-11-17 20:34:16 +0530

    feat(otpModel): Add method to validate otp
    Check if otp has expired by comparing timestamps

--- src/models/otp.model.js
+++ src/models/otp.model.js
@@ -1,23 +1,36 @@
 const mongoose = require("mongoose");
+const dayjs = require("dayjs");
 
-const otpSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    trim: true,
+const otpSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    otp: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    expiresAt: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
   },
-  otp: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  expiresAt: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-});
+  { timestamps: true },
+);
+
+// check if otp has expired
+otpSchema.methods.validateOtp = async function () {
+  const otpDocument = this;
+  const currentTimeStamp = dayjs().unix();
+  const isValid = otpDocument.expiresAt > currentTimeStamp;
+
+  return isValid;
+};
 
 const OtpModel = mongoose.model("Otp", otpSchema);
 
