234414eaba0c6b75f0a25a4423582ec29c72af23
valeyellow <sumit.mishtra.4693@gmail.com>
2021-11-18 12:05:28 +0530

    feat(auth): Handle token expired case

--- src/middleware/deserializeUser.js
+++ src/middleware/deserializeUser.js
@@ -4,19 +4,32 @@ const logger = require("../utils/logger");
 const { verifyJwt } = require("../utils/jwt.utils");
 
 // fetch accessToken from request headers -> verify and add user details to res.locals
+// eslint-disable-next-line consistent-return
 const deserializeUser = async (req, res, next) => {
   try {
     const accessToken = get(req, "headers.authorization", "").replace(
       /^Bearer\s/,
       "",
     );
+
     if (!accessToken) return next();
 
-    const { decoded } = verifyJwt(accessToken);
+    const { decoded, expired } = await verifyJwt(accessToken);
+
     if (decoded) {
       res.locals.user = decoded;
+      return next();
     }
-    return next();
+
+    // handle case where token is expired
+    if (expired) {
+      return res.status(400).send({
+        type: "error",
+        message: "You have provided a Invalid or expired token",
+      });
+    }
+
+    next();
   } catch (e) {
     logger.error(e);
   }
